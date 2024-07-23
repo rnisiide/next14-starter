c95958825c986bd4a32d34af5389994f02a73dfe
Ricardo Nisiide <rnisiide@gmail.com>
2024-07-23 14:21:07 -0300

    initial structure with navbar, footer and page links  built. css started

---
+++ src/app/(auth)/register/page.jsx
@@ -0,0 +1,5 @@
+const RegistrPage = () => {
+    return <div>RegistrPage</div>;
+};
+
+export default RegistrPage;
---
+++ src/components/navbar/links/links.jsx
@@ -0,0 +1,60 @@
+import NavLink from "./navLink/navLink";
+import styles from "./links.module.css";
+
+const Links = () => {
+
+    const links = [
+        {
+            title: "Homepage",
+            path: "/",
+
+        },
+        {
+            title: "About",
+            path: "/about",
+
+        },
+        {
+            title: "Contact",
+            path: "/contact",
+
+        },
+        {
+            title: "Blog",
+            path: "/blog",
+
+        },
+    ];
+
+    //temporary
+
+    const session = true;
+    const isAdmin = true;
+
+    return (
+        <div class={styles.links}>
+            {links.map((link =>
+                <NavLink item={link} key={link} />
+
+            ))} {session ? (
+                <>
+                    {
+                        isAdmin && (
+                            <NavLink item={{ title: "Admin", path: "/admin" }} />
+                        )
+
+                    }
+                    <button className={styles.logout}>Logout</button>
+                </>
+
+            ) : (
+                <NavLink item={{ title: "Login", path: "/login" }} />
+            )}
+        </div>
+
+    )
+};
+
+
+
+export default Links;
